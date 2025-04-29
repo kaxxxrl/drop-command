93fba24f835cf5e23c368cc5ab09f4a936bf4ac5
kaxxxrl <totalrthroandemomusics@gmail.com>
2025-04-29 12:30:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { Client, GatewayIntentBits, EmbedBuilder, SlashCommandBuilder, REST, Routes } = require('discord.js');
-const { token } = require('./config.json');
-
+const token = process.env.TOKEN;
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
